0c12c7ed493449c3f28d31fee189ab3bbe108a09
Connor-Brown <40206198+Connor-Brown@users.noreply.github.com>
2021-12-24 15:24:08 -0700

    add error page (#8)

--- src/main/java/com/mutzy/controller/AppointmentController.java
+++ src/main/java/com/mutzy/controller/AppointmentController.java
@@ -1,6 +1,5 @@
 package com.mutzy.controller;
 
-import com.mutzy.domain.Appointment;
 import com.mutzy.domain.Location;
 import com.mutzy.domain.Person;
 import com.mutzy.dto.AppointmentRequestDto;
@@ -8,6 +7,7 @@ import com.mutzy.dto.AppointmentResponseDto;
 import com.mutzy.dto.LocationDto;
 import com.mutzy.dto.PersonDto;
 import com.mutzy.service.AppointmentService;
+import com.mutzy.utils.Constants;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -16,32 +16,25 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.ValidationException;
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
 
 @Controller
 @RequestMapping("/appointments")
 @Slf4j
 public class AppointmentController {
 
-    //TODO error mapping
-
-    // if we had a larger number of views, this should be extracted to some constant file
-    protected static final String APPOINTMENTS_VIEW = "appointments";
-    private static final String MODEL_KEY_APPOINTMENT_LIST = "appointments";
-
     private final AppointmentService appointmentService;
+    private final ControllerHelper controllerHelper;
 
     @Autowired
-    public AppointmentController(AppointmentService appointmentService) {
+    public AppointmentController(AppointmentService appointmentService, ControllerHelper controllerHelper) {
         this.appointmentService = appointmentService;
+        this.controllerHelper = controllerHelper;
     }
 
     @GetMapping // ideally this would have some sort of pagination rather than returning ALL the data in our database table
     public String getAppointments(Model model) {
         log.info("Received request to get all appointments by date");
-        return redirectToAppointmentsPage(model);
+        return controllerHelper.redirectToAppointmentsPage(model);
     }
 
     @Deprecated // a POST request to a /create endpoint is redundant. Work to migrate any traffic to createAppointment() method below
@@ -65,7 +58,7 @@ public class AppointmentController {
             // This will re-query the database even though we probably already have the full list of appointments.
             // This is good to do if the user has 2 sessions open at once (unlikely, but possible)
             // but it could cause performance issues at scale (which isn't a big concern for this demo project, but it's good to be aware of)
-            return redirectToAppointmentsPage(model);
+            return controllerHelper.redirectToAppointmentsPage(model);
         } catch (ValidationException e) {
             log.warn("The given appointment failed validation", e);
             return reportAppointmentFormFailureToUser(e.getMessage(), model, appointment);
@@ -79,7 +72,7 @@ public class AppointmentController {
         } catch (Exception e) {
             log.warn(e.getMessage(), e);
         }
-        return redirectToAppointmentsPage(model);
+        return controllerHelper.redirectToAppointmentsPage(model);
     }
 
     @PostMapping("/person")
@@ -93,7 +86,7 @@ public class AppointmentController {
             if (createdPerson == null) {
                 return reportPersonFormFailureToUser("An unknown error occurred. Please reach out to support if the issue persists", model, personDto);
             }
-            return redirectToAppointmentsPage(model);
+            return controllerHelper.redirectToAppointmentsPage(model);
         } catch (ValidationException e) {
             log.warn("The given person failed validation", e);
             return reportPersonFormFailureToUser(e.getMessage(), model, personDto);
@@ -111,70 +104,34 @@ public class AppointmentController {
             if (createdLocation == null) {
                 return reportLocationFormFailureToUser("An unknown error occurred. Please reach out to support if the issue persists", model, locationDto);
             }
-            return redirectToAppointmentsPage(model);
+            return controllerHelper.redirectToAppointmentsPage(model);
         } catch (ValidationException e) {
             log.warn("The given location failed validation", e);
             return reportLocationFormFailureToUser(e.getMessage(), model, locationDto);
         }
     }
 
-    private String redirectToAppointmentsPage(Model model) {
-        populateAllAppointments(model);
-        ensureRequiredFieldsArePopulated(model);
-        return APPOINTMENTS_VIEW;
-    }
-
-    private void populateAllAppointments(Model model) {
-        List<AppointmentResponseDto> appointments = Optional.ofNullable(appointmentService.findAllAppointments())
-                .orElse(Collections.emptyList());
-        log.info("Found {} appointments", appointments.size());
-        model.addAttribute(MODEL_KEY_APPOINTMENT_LIST, appointments);
-    }
-
-    private void ensureRequiredFieldsArePopulated(Model model) {
-        resetFormObjects(model);
-        populateAllPeople(model);
-        populateAllLocations(model);
-    }
-
-    private void resetFormObjects(Model model) {
-        model.addAttribute("appointment", new AppointmentRequestDto());
-        model.addAttribute("person", new PersonDto());
-        model.addAttribute("location", new LocationDto());
-        model.addAttribute("appointmentValidationError", "");
-        model.addAttribute("personValidationError", "");
-        model.addAttribute("locationValidationError", "");
-    }
-
-    private void populateAllPeople(Model model) {
-        model.addAttribute("people", appointmentService.findAllPeople());
-    }
-
-    private void populateAllLocations(Model model) {
-        model.addAttribute("locations", appointmentService.findAllLocations());
-    }
-
     private String reportAppointmentFormFailureToUser(String errorMessage, Model model, AppointmentRequestDto appointmentRequestDto) {
-        populateAllAppointments(model);
-        ensureRequiredFieldsArePopulated(model);
+        controllerHelper.populateAllAppointments(model);
+        controllerHelper.ensureRequiredFieldsArePopulated(model);
         model.addAttribute("appointment", appointmentRequestDto);
         model.addAttribute("appointmentValidationError", errorMessage);
-        return APPOINTMENTS_VIEW;
+        return Constants.APPOINTMENTS_PAGE;
     }
 
     private String reportPersonFormFailureToUser(String errorMessage, Model model, PersonDto personDto) {
-        populateAllAppointments(model);
-        ensureRequiredFieldsArePopulated(model);
+        controllerHelper.populateAllAppointments(model);
+        controllerHelper.ensureRequiredFieldsArePopulated(model);
         model.addAttribute("person", personDto);
         model.addAttribute("personValidationError", errorMessage);
-        return APPOINTMENTS_VIEW;
+        return Constants.APPOINTMENTS_PAGE;
     }
 
     private String reportLocationFormFailureToUser(String errorMessage, Model model, LocationDto locationDto) {
-        populateAllAppointments(model);
-        ensureRequiredFieldsArePopulated(model);
+        controllerHelper.populateAllAppointments(model);
+        controllerHelper.ensureRequiredFieldsArePopulated(model);
         model.addAttribute("location", locationDto);
         model.addAttribute("locationValidationError", errorMessage);
-        return APPOINTMENTS_VIEW;
+        return Constants.APPOINTMENTS_PAGE;
     }
 }
